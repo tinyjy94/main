976cfbd224714ed35524e9945726b9d9a44fced7
qwlai <laiqiwei30@hotmail.com>
2018-03-06 22:38:07 +0800

    Add alias for list command

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -71,6 +71,7 @@ public class AddressBookParser {
         case FindCommand.COMMAND_WORD:
             return new FindCommandParser().parse(arguments);
 
+        case ListCommand.COMMAND_ALIAS:
         case ListCommand.COMMAND_WORD:
             return new ListCommand();
 
