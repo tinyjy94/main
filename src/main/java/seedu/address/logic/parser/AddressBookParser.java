fb8953925fd4410ffd1fcc398b498d2217422c84
qwlai <laiqiwei30@hotmail.com>
2018-03-06 22:48:50 +0800

    Add alias for undo command

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -87,6 +87,7 @@ public class AddressBookParser {
         case HelpCommand.COMMAND_WORD:
             return new HelpCommand();
 
+        case UndoCommand.COMMAND_ALIAS:
         case UndoCommand.COMMAND_WORD:
             return new UndoCommand();
 
