ed2688affc761f334688ae54112c2cf959e04eb2
tinyjy94 <tiny_jy@hotmail.com>
2018-03-25 22:29:20 +0800

    add Test for addTheaterCommand

---
+++ src/test/java/seedu/address/logic/commands/AddTheaterCommandTest.java
@@ -0,0 +1,81 @@
+package seedu.address.logic.commands;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NUMOFNEWTHEATERS;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NUMOFTHEATERS;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.prepareUndoCommand;
+import static seedu.address.testutil.TypicalCinemas.getTypicalMoviePlanner;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_CINEMA;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_CINEMA;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.UndoRedoStack;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.MoviePlanner;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.cinema.Cinema;
+import seedu.address.model.cinema.Theater;
+import seedu.address.testutil.CinemaBuilder;
+
+/**
+ * Contains integration tests (interaction with the Model, UndoCommand and RedoCommand) and unit tests for EditCommand.
+ */
+
+public class AddTheaterCommandTest {
+    private Model model = new ModelManager(getTypicalMoviePlanner(), new UserPrefs());
+
+    @Test
+    public void executeUndo_validIndexUnfilteredList_success() throws Exception {
+        UndoRedoStack undoRedoStack = new UndoRedoStack();
+        UndoCommand undoCommand = prepareUndoCommand(model, undoRedoStack);
+        Cinema resizedCinema = new CinemaBuilder().build();
+        AddTheaterCommand addTheaterCommand = prepareCommand(INDEX_FIRST_CINEMA, resizedCinema);
+        Model expectedModel = new ModelManager(new MoviePlanner(model.getMoviePlanner()), new UserPrefs());
+
+        // edit -> first cinema edited
+        addTheaterCommand.execute();
+        undoRedoStack.push(addTheaterCommand);
+
+        // undo -> reverts movieplanner back to previous state and filtered cinema list to show all cinemas
+        assertCommandSuccess(undoCommand, model, UndoCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
+    @Test
+    public void equals() {
+        ArrayList<Theater> firstTheater = new ArrayList<>(VALID_NUMOFTHEATERS);
+        ArrayList<Theater> secondTheater = new ArrayList<>(VALID_NUMOFNEWTHEATERS);
+        ArrayList<Theater> thirdTheater = new ArrayList<>(VALID_NUMOFTHEATERS);
+        AddTheaterCommand firstTheaterCommand = new AddTheaterCommand(INDEX_FIRST_CINEMA, firstTheater.size());
+        AddTheaterCommand secondTheaterCommand = new AddTheaterCommand(INDEX_SECOND_CINEMA, secondTheater.size());
+        AddTheaterCommand thirdTheaterCommand = new AddTheaterCommand(INDEX_FIRST_CINEMA, thirdTheater.size());
+
+        // null -> returns false
+        assertFalse(firstTheaterCommand.equals(null));
+
+        // not equals -> returns false
+        assertFalse(firstTheaterCommand.equals(secondTheaterCommand));
+
+        // same value -> returns true
+        assertTrue(firstTheaterCommand.equals(thirdTheaterCommand));
+
+        // same object -> returns true
+        assertTrue(firstTheaterCommand.equals(firstTheaterCommand));
+    }
+
+    /**
+     * Returns an {@code EditCommand} with parameters {@code index} and {@code descriptor}
+     */
+    private AddTheaterCommand prepareCommand (Index index, Cinema cinema) {
+        AddTheaterCommand addTheaterCommand = new AddTheaterCommand(index, cinema.getTheaters().size());
+        addTheaterCommand.setData(model, new CommandHistory(), new UndoRedoStack());
+        return addTheaterCommand;
+    }
+}
