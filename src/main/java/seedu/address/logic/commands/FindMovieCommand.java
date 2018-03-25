58a47772618269918138b87663df48a5ee95a841
slothhy <ohy1994@gmail.com>
2018-03-26 03:30:33 +0800

    Add parameter to FindMovie command to support name search

--- src/main/java/seedu/address/logic/commands/FindMovieCommand.java
+++ src/main/java/seedu/address/logic/commands/FindMovieCommand.java
@@ -1,6 +1,16 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import seedu.address.model.movie.MovieName;
 import seedu.address.model.movie.NameContainsKeywordsPredicate;
+import seedu.address.model.movie.StartDate;
+import seedu.address.model.tag.Tag;
 
 /**
  * Finds and lists all movies in movie planner whose name contains any of the argument keywords.
@@ -12,9 +22,10 @@ public class FindMovieCommand extends Command {
     public static final String COMMAND_ALIAS = "fm";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all movies whose names contain any of "
-            + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
-            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " avenger horror nemo";
+            + "the specified keywords (case-sensitive).\n"
+            + "Parameters: "
+            + PREFIX_NAME + "KEYWORDS "
+            + "Example: " + COMMAND_WORD + " "+ PREFIX_NAME + " avenger horror nemo";
 
     private final NameContainsKeywordsPredicate predicate;
 
@@ -33,5 +44,82 @@ public class FindMovieCommand extends Command {
         return other == this // short circuit if same object
                 || (other instanceof FindMovieCommand // instanceof handles nulls
                 && this.predicate.equals(((FindMovieCommand) other).predicate)); // state check
+    }
+
+    /**
+     * Stores the details for movies to find. Each non-empty field value will
+     * be the search conditions.
+     */
+    public static class FindMovieDescriptor {
+        private MovieName movieName;
+        private StartDate startDate;
+        private Set<Tag> tags;
+
+        public FindMovieDescriptor() {}
+
+        /**
+         * Copy constructor.
+         * A defensive copy of {@code tags} is used internally.
+         */
+        public FindMovieDescriptor(FindMovieDescriptor toCopy) {
+            setName(toCopy.movieName);
+            setStartDate(toCopy.startDate);
+            setTags(toCopy.tags);
+        }
+
+
+        public void setName(MovieName movieName) {
+            this.movieName = movieName;
+        }
+
+        public Optional<MovieName> getName() {
+            return Optional.ofNullable(movieName);
+        }
+
+        public void setStartDate(StartDate startDate) {
+            this.startDate = startDate;
+        }
+
+        public Optional<StartDate> getStartDate() {
+            return Optional.ofNullable(startDate);
+        }
+
+        /**
+         * Sets {@code tags} to this object's {@code tags}.
+         * A defensive copy of {@code tags} is used internally.
+         */
+        public void setTags(Set<Tag> tags) {
+            this.tags = (tags != null) ? new HashSet<>(tags) : null;
+        }
+
+        /**
+         * Returns an unmodifiable tag set, which throws {@code UnsupportedOperationException}
+         * if modification is attempted.
+         * Returns {@code Optional#empty()} if {@code tags} is null.
+         */
+        public Optional<Set<Tag>> getTags() {
+            return (tags != null) ? Optional.of(Collections.unmodifiableSet(tags)) : Optional.empty();
+        }
+
+
+        @Override
+        public boolean equals(Object other) {
+            // short circuit if same object
+            if (other == this) {
+                return true;
+            }
+
+            // instanceof handles nulls
+            if (!(other instanceof FindMovieDescriptor)) {
+                return false;
+            }
+
+            // state check
+            FindMovieDescriptor e = (FindMovieDescriptor) other;
+
+            return getName().equals(e.getName())
+                    && getStartDate().equals(e.getStartDate())
+                    && getTags().equals(e.getTags());
+        }
     }
 }
--- src/main/java/seedu/address/logic/parser/FindMovieCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindMovieCommandParser.java
@@ -1,11 +1,15 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 
+import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.FindMovieCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.movie.MovieName;
 import seedu.address.model.movie.NameContainsKeywordsPredicate;
 
 /**
@@ -19,15 +23,34 @@ public class FindMovieCommandParser implements Parser<FindMovieCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindMovieCommand parse(String args) throws ParseException {
+
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_NAME)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindMovieCommand.MESSAGE_USAGE));
+        }
+
         String trimmedArgs = args.trim();
         if (trimmedArgs.isEmpty()) {
             throw new ParseException(
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindMovieCommand.MESSAGE_USAGE));
+        } else {
+            trimmedArgs = argMultimap.getValue(PREFIX_NAME).get();
         }
 
         String[] nameKeywords = trimmedArgs.split("\\s+");
 
         return new FindMovieCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 
 }
